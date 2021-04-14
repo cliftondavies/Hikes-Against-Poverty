d38626b788c73807f04959c591932dcc815b2995
cliftondavies <clifton.davies@outlook.com>
2021-04-14 20:45:46 +0100

    Define redux actions

---
+++ src/redux/actions.js
@@ -0,0 +1,40 @@
+export const LOAD_HIKES = 'LOAD_HIKES';
+export const LOAD_BOOKINGS = 'LOAD_BOOKINGS';
+export const UPDATE_BOOKING_STATUS = 'UPDATE_BOOKING_STATUS';
+export const LOAD_HIKE_ERROR = 'LOAD_ERROR';
+export const LOAD_BOOKING_ERROR = 'LOAD_BOOKING_ERROR';
+
+export const loadHikes = ({ hikes }) => (
+  {
+    type: LOAD_HIKES,
+    payload: { hikes },
+  }
+);
+
+export const loadBookings = ({ bookings }) => (
+  {
+    type: LOAD_BOOKINGS,
+    payload: { bookings },
+  }
+);
+
+export const updateBookingStatus = (status) => (
+  {
+    type: UPDATE_BOOKING_STATUS,
+    payload: { status },
+  }
+);
+
+export const loadHikeError = ({ error }) => (
+  {
+    type: LOAD_HIKE_ERROR,
+    payload: { error },
+  }
+);
+
+export const loadBookingError = ({ error }) => (
+  {
+    type: LOAD_BOOKING_ERROR,
+    payload: { error },
+  }
+);
