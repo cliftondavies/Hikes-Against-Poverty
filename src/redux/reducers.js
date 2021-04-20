7eea84a71ad9ebc7193b9266051b5811949a5c48
cliftondavies <clifton.davies@outlook.com>
2021-04-20 23:35:22 +0100

    Update redux reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import {
-  LOAD_HIKES, LOAD_BOOKINGS, UPDATE_BOOKING_STATUS, LOAD_HIKE_ERROR, LOAD_BOOKING_ERROR,
+  LOAD_HIKES, LOAD_BOOKINGS, LOAD_HIKE_ERROR, LOAD_BOOKING_ERROR,
+  RESET_STORE,
 } from './actions';
 
 export const hikesInitialState = {
@@ -12,7 +13,7 @@ export const hikesInitialState = {
 export const bookingsInitialState = {
   bookings: [],
   loading: 'idle',
-  newBooking: false,
+  // newBooking: false,
   error: null,
 };
 
@@ -25,13 +26,14 @@ export const hikes = (state = hikesInitialState, action) => {
         defaultLoading: 'completed',
         error: null,
       };
-    case LOAD_HIKE_ERROR: {
+    case LOAD_HIKE_ERROR:
       return {
         ...state,
         loading: 'failed',
         error: action.payload.error,
       };
-    }
+    case RESET_STORE:
+      return hikesInitialState;
     default:
       return state;
   }
@@ -46,21 +48,37 @@ export const bookings = (state = bookingsInitialState, action) => {
         loading: 'completed',
         error: null,
       };
-    case UPDATE_BOOKING_STATUS:
-      return {
-        ...state,
-        newBooking: action.payload.status,
-      };
-    case LOAD_BOOKING_ERROR: {
+    // case UPDATE_BOOKING_STATUS:
+    //   return {
+    //     ...state,
+    //     newBooking: action.payload.status,
+    //   };
+    case LOAD_BOOKING_ERROR:
       return {
         ...state,
         loading: 'failed',
         error: action.payload.error,
       };
-    }
+    case RESET_STORE:
+      return bookingsInitialState;
     default:
       return state;
   }
 };
 
 export default combineReducers({ hikes, bookings });
+// const appReducer = combineReducers({ hikes, bookings });
+
+// const rootReducer = (state, action) => {
+//   let newState;
+
+//   if (action.type === RESET_STORE) {
+//     // state = undefined;
+//     newState = state;
+//     newState = undefined;
+//   }
+
+//   return appReducer(newState, action);
+// };
+
+// export default rootReducer;
