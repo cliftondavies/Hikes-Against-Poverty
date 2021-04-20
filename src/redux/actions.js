489a9817f666a3db65faf66a7a14d7fcda36db1e
cliftondavies <clifton.davies@outlook.com>
2021-04-20 23:34:53 +0100

    Update redux actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,9 @@
 export const LOAD_HIKES = 'LOAD_HIKES';
 export const LOAD_BOOKINGS = 'LOAD_BOOKINGS';
-export const UPDATE_BOOKING_STATUS = 'UPDATE_BOOKING_STATUS';
+// export const UPDATE_BOOKING_STATUS = 'UPDATE_BOOKING_STATUS';
 export const LOAD_HIKE_ERROR = 'LOAD_ERROR';
 export const LOAD_BOOKING_ERROR = 'LOAD_BOOKING_ERROR';
+export const RESET_STORE = 'RESET_STORE';
 
 export const loadHikes = ({ hikes }) => (
   {
@@ -18,12 +19,12 @@ export const loadBookings = ({ bookings }) => (
   }
 );
 
-export const updateBookingStatus = (status) => (
-  {
-    type: UPDATE_BOOKING_STATUS,
-    payload: { status },
-  }
-);
+// export const updateBookingStatus = (status) => (
+//   {
+//     type: UPDATE_BOOKING_STATUS,
+//     payload: { status },
+//   }
+// );
 
 export const loadHikeError = ({ error }) => (
   {
@@ -36,5 +37,11 @@ export const loadBookingError = ({ error }) => (
   {
     type: LOAD_BOOKING_ERROR,
     payload: { error },
+  }
+);
+
+export const resetStore = () => (
+  {
+    type: RESET_STORE,
   }
 );
