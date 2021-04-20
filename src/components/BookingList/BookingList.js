298ebc73bf61bcd3c05360684da9125c15e28fab
cliftondavies <clifton.davies@outlook.com>
2021-04-20 23:40:36 +0100

    Implement the booking list component

---
+++ src/components/BookingList/BookingList.js
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import BookingCard from '../BookingCard/BookingCard';
+import { userBookings } from '../../api/api';
+
+const BookingList = () => {
+  const { bookings, loading, error } = useSelector((state) => state.bookings);
+  const dispatch = useDispatch();
+  const extraBookingCards = 9 - bookings.length;
+
+  const padBookings = (difference) => {
+    const result = [];
+    for (let i = 0; i < difference; i += 1) {
+      result.push(<BookingCard key={i} />);
+    }
+  };
+
+  useEffect(() => {
+    if (loading === 'idle' && JSON.parse(sessionStorage.getItem('user'))) {
+      const storedResponse = JSON.parse(sessionStorage.getItem('user'));
+      const {
+        accessToken, uid, client, tokenType, expiry,
+      } = storedResponse.authentication;
+      dispatch(userBookings({
+        accessToken, uid, client, tokenType, expiry,
+      }));
+    }
+  }, [loading, dispatch]);
+
+  return (
+    <div>
+      {(bookings && bookings.length > 0) ? bookings.map((booking) => (
+        <BookingCard booking={booking} key={booking.id} />
+      )) : <span>{error}</span>}
+      {(extraBookingCards > 0) ? padBookings(extraBookingCards) : null}
+    </div>
+  );
+};
+
+export default BookingList;
