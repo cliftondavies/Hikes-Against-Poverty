919b2196e0740d75aeb79b4ecdf8736f0c593e16
cliftondavies <clifton.davies@outlook.com>
2021-04-20 23:39:53 +0100

    Implement the booking card component

---
+++ src/components/BookingCard/BookingCard.js
@@ -0,0 +1,36 @@
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const BookingCard = ({ booking }) => {
+  const { date, city, hike_id: id } = booking;
+  const hike = useSelector((state) => state.hikes.hikes.find((hike) => hike.id === id));
+
+  const articleInfo = () => (
+    <article>
+      <h3>Type</h3>
+      <span>{hike.name}</span>
+      <h3>Date</h3>
+      <span>{date}</span>
+      <h3>Location</h3>
+      <span>{city}</span>
+    </article>
+  );
+
+  return (
+    (booking) ? articleInfo() : (
+      <article>
+        <span>Empty</span>
+      </article>
+    )
+  );
+};
+
+BookingCard.defaultProps = {
+  booking: false,
+};
+
+BookingCard.propTypes = {
+  booking: PropTypes.objectOf(PropTypes.any),
+};
+
+export default BookingCard;
