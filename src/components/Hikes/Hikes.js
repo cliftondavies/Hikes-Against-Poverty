bc140295285fc7f203df254c1ee82e481784f82e
cliftondavies <clifton.davies@outlook.com>
2021-04-20 23:36:30 +0100

    Create hikes page

---
+++ src/components/Hikes/Hikes.js
@@ -0,0 +1,35 @@
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import HikeList from '../HikeList/HikeList';
+import Hike from '../Hike/Hike';
+
+const Hikes = () => {
+  const { path } = useRouteMatch();
+  const { hikes } = useSelector((state) => state.hikes);
+
+  return (
+    <Switch>
+      <Route exact path={path}>
+        <main>
+          <div><span role="img" aria-label="play forward">▶️ &#9657;</span></div>
+          <div><span role="img" aria-label="play backward">◀️ &#9667;</span></div>
+
+          <h2>AVAILABLE HIKES</h2>
+
+          <span>Please select a hike</span>
+
+          <HikeList />
+        </main>
+      </Route>
+      <Route
+        path={`${path}/:hikeID`}
+        render={({ match }) => (
+          <Hike hike={hikes.find((hike) => hike.id === Number(match.params.hikeID))} />
+        )}
+      />
+      <Route render={() => <h1>404: Page Not Found!</h1>} />
+    </Switch>
+  );
+};
+
+export default Hikes;
