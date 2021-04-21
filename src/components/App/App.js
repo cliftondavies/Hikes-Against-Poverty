20d9ec9e48c1fe9b0b117d7bd432c8bb58568a2b
cliftondavies <clifton.davies@outlook.com>
2021-04-21 22:33:49 +0100

    Implement user session tracking in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,41 @@
+import { useState, useEffect } from 'react';
 import {
   BrowserRouter, Switch, Route, Redirect,
 } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from '../../redux/actions';
 import SideBar from '../SideBar/SideBar';
 import Home from '../Home/Home';
 import Hikes from '../Hikes/Hikes';
 import Bookings from '../Bookings/Bookings';
 
 const App = () => {
+  const [session, setSession] = useState(JSON.parse(sessionStorage.getItem('user')));
   const loggedIn = useSelector((state) => state.authenticated);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (JSON.parse(sessionStorage.getItem('user'))) {
+      dispatch(login());
+    }
+  }, []);
+
+  useEffect(() => {
+    setSession(JSON.parse(sessionStorage.getItem('user')));
+  }, [loggedIn]);
 
   return (
     <BrowserRouter>
       <SideBar />
       <Switch>
         <Route exact path="/">
-          {loggedIn ? <Redirect to="/hikes" /> : <Home />}
+          {(loggedIn || session) ? <Redirect to="/hikes" /> : <Home />}
         </Route>
         <Route path="/hikes">
-          {!loggedIn ? <Redirect to="/" /> : <Hikes />}
+          {(!loggedIn && !session) ? <Redirect to="/" /> : <Hikes />}
         </Route>
         <Route path="/bookings">
-          {!loggedIn ? <Redirect to="/" /> : <Bookings />}
+          {(!loggedIn && !session) ? <Redirect to="/" /> : <Bookings />}
         </Route>
         <Route render={() => <h1>404: Page Not Found!</h1>} />
       </Switch>
