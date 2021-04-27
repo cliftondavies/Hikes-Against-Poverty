1c2eed32639c4b46185550b54ba469ff61064320
cliftondavies <clifton.davies@outlook.com>
2021-04-27 22:18:36 +0100

    Implement styling for sign up, sign in and booking forms

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,11 +2,12 @@ import PropTypes from 'prop-types';
 import SignUpForm from '../SignUpForm/SignUpForm';
 import SignInForm from '../SignInForm/SignInForm';
 import BookingForm from '../BookingForm/BookingForm';
+import styles from './Form.module.scss';
 
 const Form = ({ formType, status, formHandler }) => {
-  if (formType === 'Sign Up') { return <SignUpForm active={status} formHandler={formHandler} />; }
-  if (formType === 'Sign In') { return <SignInForm active={status} formHandler={formHandler} />; }
-  if (formType === 'Book') { return <BookingForm active={status} />; }
+  if (formType === 'Sign Up') { return <SignUpForm active={status} formHandler={formHandler} style={styles.form} />; }
+  if (formType === 'Sign In') { return <SignInForm active={status} formHandler={formHandler} style={styles.form} />; }
+  if (formType === 'Book') { return <BookingForm active={status} style={styles.form} />; }
   return null;
 };
 
