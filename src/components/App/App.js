a99ad5fe462811f18c68ae92f3d87986eb0bcb96
cliftondavies <clifton.davies@outlook.com>
2021-04-29 20:29:57 +0100

    Move all routing to the base App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from 'react';
 import {
-  HashRouter, Switch, Route, Redirect,
+  HashRouter, Switch, Route, useRouteMatch, Redirect,
 } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { login } from '../../redux/actions';
 import SideBar from '../SideBar/SideBar';
 import Home from '../Home/Home';
 import Hikes from '../Hikes/Hikes';
+import Hike from '../Hike/Hike';
 import Bookings from '../Bookings/Bookings';
 
 const App = () => {
   const [session, setSession] = useState(JSON.parse(sessionStorage.getItem('user')));
+  const { path } = useRouteMatch();
   const loggedIn = useSelector((state) => state.authenticated);
+  const { hikes } = useSelector((state) => state.hikes);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -32,7 +35,20 @@ const App = () => {
           {(loggedIn || session) ? <Redirect to="/hikes" /> : <Home />}
         </Route>
         <Route path="/hikes">
-          {(!loggedIn && !session) ? <Redirect to="/" /> : <Hikes />}
+          {(!loggedIn && !session) ? <Redirect to="/" /> : (
+            <Switch>
+              <Route exact path={path}>
+                <Hikes />
+              </Route>
+              <Route
+                path={`${path}/:hikeID`}
+                render={({ match }) => (
+                  <Hike hike={hikes.find((hike) => hike.id === Number(match.params.hikeID))} />
+                )}
+              />
+              <Route render={() => <h1>404: Page Not Found!</h1>} />
+            </Switch>
+          )}
         </Route>
         <Route path="/bookings">
           {(!loggedIn && !session) ? <Redirect to="/" /> : <Bookings />}
--- src/components/Hikes/Hikes.js
+++ src/components/Hikes/Hikes.js
@@ -1,35 +1,21 @@
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import HikeList from '../HikeList/HikeList';
-import Hike from '../Hike/Hike';
 import styles from './Hikes.module.scss';
 
 const Hikes = () => {
-  const { path } = useRouteMatch();
   const { hikes } = useSelector((state) => state.hikes);
 
   return (
-    <Switch>
-      <Route exact path={path}>
-        <main className={styles.hikes}>
-          <span>&#9657;</span>
-          <span>&#9667;</span>
+    <main className={styles.hikes}>
+      <span>&#9657;</span>
+      <span>&#9667;</span>
 
-          <h2>AVAILABLE HIKES</h2>
+      <h2>AVAILABLE HIKES</h2>
 
-          <span>Please select a hike</span>
+      <span>Please select a hike</span>
 
-          <HikeList hikes={hikes} />
-        </main>
-      </Route>
-      <Route
-        path={`${path}/:hikeID`}
-        render={({ match }) => (
-          <Hike hike={hikes.find((hike) => hike.id === Number(match.params.hikeID))} />
-        )}
-      />
-      <Route render={() => <h1>404: Page Not Found!</h1>} />
-    </Switch>
+      <HikeList hikes={hikes} />
+    </main>
   );
 };
 
